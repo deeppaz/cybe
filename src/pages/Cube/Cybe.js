21988a1be07714b28338dd7aef6573d01a9dff24
jimmy durak <deeppaz@hotmail.com>
2021-11-17 16:34:55 +0300

    loading added

--- src/pages/Cube/Cybe.js
+++ src/pages/Cube/Cybe.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, Suspense, useEffect } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
-import { useGLTF, OrbitControls, ContactShadows, Environment } from "@react-three/drei"
+import { useGLTF, OrbitControls, ContactShadows, Environment, Html, useProgress } from "@react-three/drei"
 import { proxy, useSnapshot } from "valtio"
 import { HexColorPicker } from "react-colorful"
 
@@ -57,6 +57,11 @@ function Cube() {
   )
 }
 
+function Loader() {
+  const { loaded } = useProgress()
+  return <Html style={{fontWeight:"bolder"}} center>{loaded}% loaded</Html>
+}
+
 function Picker() {
   const snap = useSnapshot(state)
 
@@ -74,7 +79,7 @@ function Cybe() {
       <Canvas shadows dpr={[1, 2]} camera={{ position: [12, 12, 20], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <spotLight intensity={0.3} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
-        <Suspense fallback={null}>
+        <Suspense fallback={<Loader />}>
           <Cube />
           <Environment files="hdr.hdr" />
           <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.8, 0]} opacity={0.25} width={10} height={10} blur={2} far={1} />
